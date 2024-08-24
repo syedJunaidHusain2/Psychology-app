ea47c24660962b4973c89c6b1e0f2ce7da848a8c
mohdkhalid26 <mohdkhalid211003@gmail.com>
2024-08-24 17:02:40 +0530

    complete header animations but some works pending

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,27 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [navOpt, setNavOpt] = useState(["Home", "Services", "Blog", "Contact"]);
   return (
     <nav className=" absolute top-0 font-title min-w-full max-w-full  p-2.5 flex items-center justify-center z-10 text-white gap-16 ">
-      <span className=" text-center text-lg font-bold cursor-pointer select-none">
-        Home
-      </span>
-      <span className=" text-center text-lg font-bold cursor-pointer select-none">
-        Services
-      </span>
-      <span className=" text-center text-lg font-bold cursor-pointer select-none">
-        Blog
-      </span>
-      <span className=" text-center text-lg font-bold cursor-pointer select-none">
-        Contact
-      </span>
+      {navOpt.map((opt, i) => {
+        return (
+          <span
+            key={i}
+            className=" hover:text-green-700 text-center text-lg font-bold cursor-pointer select-none"
+          >
+            {opt}
+          </span>
+        );
+      })}
       <div className=" flex items-center flex-col justify-center gap-1">
         <span className=" text-center text-sm font-bold select-none">
           Need Help?
         </span>
         <span className=" text-center text-lg font-bold">+91 8303388143</span>
       </div>
-      <button className=" text-green-100 border-none rounded-3xl text-center bg-green-700 px-6 py-2 text-sm font-bold cursor-pointer select-none">
+      <button className=" text-green-100 border-none rounded-3xl text-center bg-green-700 px-6 py-2 text-sm font-bold cursor-pointer select-none hover:text-green-700 hover:bg-white">
         Make Appointment
       </button>
     </nav>
--- components/swiper/index.tsx
+++ components/swiper/index.tsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-
+import laughImg1 from "../../public/laugh_images/1.svg";
+import laughImg2 from "../../public/laugh_images/2.svg";
+import laughImg3 from "../../public/laugh_images/3.svg";
+import laughImg4 from "../../public/laugh_images/4.svg";
+import laughImg6 from "../../public/laugh_images/6.svg";
+import laughImg7 from "../../public/laugh_images/7.svg";
+import laughImg8 from "../../public/laugh_images/8.svg";
+import laughImg9 from "../../public/laugh_images/9.svg";
+import laughImg10 from "../../public/laugh_images/10.svg";
 import "./styles.css";
 
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 export default function SwiperApp() {
+  const [laughImages, setLaughImages] = useState([
+    laughImg6,
+    laughImg8,
+    laughImg9,
+    laughImg10,
+    laughImg1,
+    laughImg2,
+    laughImg3,
+    laughImg4,
+  ]);
   return (
     <>
       <Swiper
@@ -18,13 +36,36 @@ export default function SwiperApp() {
           delay: 5000,
           disableOnInteraction: false,
         }}
-        speed={1500}
+        speed={500}
         navigation={true}
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide className="first-swiper"></SwiperSlide>
-        <SwiperSlide className="second-swiper"></SwiperSlide>
+        <SwiperSlide className="first-swiper">
+          {laughImages.map((imgs, i) => {
+            return (
+              <img
+                src={imgs.src}
+                key={i}
+                className={`laugh-img${i + 1}`}
+                alt=""
+              />
+            );
+          })}
+        </SwiperSlide>
+        <SwiperSlide className="second-swiper">
+          <img
+            className="img1"
+            src="https://themewant.com/products/wordpress/mindthera/wp-content/uploads/2024/06/flowers-crop.webp"
+            alt=""
+          />
+          <img
+            className="img2"
+            src="https://themewant.com/products/wordpress/mindthera/wp-content/uploads/2024/06/flowers-crop.webp"
+            alt=""
+          />
+          <img className="img3" src={laughImg7.src} alt="" />
+        </SwiperSlide>
       </Swiper>
     </>
   );
