5e22c6d8ef4ccb44a9d2f96aefd4d424160490f6
mohdkhalid26 <mohdkhalid211003@gmail.com>
2024-08-25 20:29:34 +0530

    khalid-commit

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,23 +6,64 @@ import img4 from "../../public/welcome_sec_images/img4.jpg";
 import img5 from "../../public/welcome_sec_images/img5.jpg";
 import img6 from "../../public/welcome_sec_images/img6.jpg";
 import "./style.css";
+import { useEffect, useState } from "react";
 const Home = () => {
+  const [scrollCon1, setScrollCon1] = useState({
+    img3Class: "",
+    img4Class: "",
+    img5Class: "",
+    img6Class: "",
+  });
+  useEffect(() => {
+    const funcForAddClassInImgForCon = () => {
+      if (window.scrollY >= 563) {
+        setScrollCon1({
+          img3Class: "animate-img3",
+          img4Class: "animate-img4",
+          img5Class: "animate-img5",
+          img6Class: "animate-img6",
+        });
+      }
+    };
+    window.addEventListener("scroll", funcForAddClassInImgForCon);
+  }, []);
+
   return (
     <div className="home">
       <Navbar />
       <SwiperApp />
-      <main className="flex min-h-screen w-full items-center justify-center gap-10">
-        <div className="flex w-2/4 items-center justify-end gap-5 ">
-          <div className="flex items-end justify-end flex-col gap-5 mt-9">
-            <img className="rounded-xl" width="180px" src={img3.src} alt="" />
-            <img className="rounded-xl" width="270px" src={img4.src} alt="" />
+      <main className="welcome-section-main-div flex min-h-screen w-full items-center justify-center gap-10">
+        <div className="left-div-of-welcome-section-main-div flex w-2/4 items-center justify-end gap-5 ">
+          <div className="first-img-div-of-welcome-section-main-div flex items-end justify-end flex-col gap-5 mt-9">
+            <img
+              className={`rounded-xl ${scrollCon1.img3Class}`}
+              width="180px"
+              src={img3.src}
+              alt=""
+            />
+            <img
+              className={`rounded-xl ${scrollCon1.img4Class}`}
+              width="270px"
+              src={img4.src}
+              alt=""
+            />
           </div>
-          <div className="flex flex-col items-start justify-start gap-5 mb-9">
-            <img className="rounded-xl" width="270px" src={img5.src} alt="" />
-            <img className="rounded-xl" width="180px" src={img6.src} alt="" />
+          <div className="second-img-div-of-welcome-section-main-div flex flex-col items-start justify-start gap-5 mb-9">
+            <img
+              className={`rounded-xl ${scrollCon1.img5Class}`}
+              width="270px"
+              src={img5.src}
+              alt=""
+            />
+            <img
+              className={`rounded-xl ${scrollCon1.img6Class}`}
+              width="180px"
+              src={img6.src}
+              alt=""
+            />
           </div>
         </div>
-        <div className="flex w-2/4 flex-col items-start justify-center gap-2 ">
+        <div className="right-div-of-welcome-section-main-div flex w-2/4 flex-col items-start justify-center gap-2 ">
           <span className="rounded-full bg-green-200 px-5 py-1 font-bold text-black">
             Welcome
           </span>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import Home from "./home/page";
 
 export default function App() {
-  return (
- <>
- <Home/>
- </>
-  );
+  return <Home />;
 }
