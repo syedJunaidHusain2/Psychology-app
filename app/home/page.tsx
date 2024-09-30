c3012e23b5a8cbf02d03c4f2667b7b8e446acbb5
mohdkhalid26 <mohdkhalid211003@gmail.com>
2024-10-01 00:47:32 +0530

    issue-home"div resolved"

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -15,21 +15,21 @@ import IndividualSec from "@/components/individual_therapy";
 import SwiperApp2 from "@/components/swiper2";
 const Home = () => {
   return (
-    <>
+    <div className="home">
       <Navbar />
       <SwiperApp />
-      <WelcomeSec/>
+      <WelcomeSec />
       <TherapistAndTreatment />
-<IndividualSec/>
-<SwiperApp2/>
-       <IncreaseCounter />
+      <IndividualSec />
+      <SwiperApp2 />
+      <IncreaseCounter />
       <TeamSec />
       <PricingSec />
       <QuestionSec />
       <BottomSec />
       <Footer />
       <Copyright />
-    </>
+    </div>
   );
 };
 
