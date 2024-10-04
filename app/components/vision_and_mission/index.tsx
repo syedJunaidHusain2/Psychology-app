4019612613850d4b64c38c398554db8e0386e9a7
mohdkhalid26 <mohdkhalid211003@gmail.com>
2024-10-04 20:18:02 +0530

    completed responsiveness to 300px

--- app/components/vision_and_mission/index.tsx
+++ app/components/vision_and_mission/index.tsx
@@ -4,23 +4,23 @@ import { visionAndMissionContent } from "../../data/Vision&MissionData";
 function VisionAndMission() {
   const [visionAndMissionContentData] = useState(visionAndMissionContent);
   return (
-    <main className=" w-full flex items-center justify-center py-16">
+    <main className=" w-full flex items-center justify-center py-16 media-max-900:pt-0">
       <div className="vision-and-mission-container max-w-[1400px] w-full flex items-center justify-center">
-        <div className="vision-and-mission w-[82%] flex items-center justify-between flex-col gap-8">
+        <div className="vision-and-mission w-[82%] flex items-center justify-between flex-col gap-8 max-md:gap-4">
           <div className="top-div-of-vision-and-mission w-full flex flex-col items-center justify-center gap-4">
-            <span className=" text-base underline jost_regular font-bold text-black">
+            <span className=" text-base underline jost_regular font-bold text-black max-md:text-sm">
               Vision & Mission
             </span>
-            <h3 className=" text-4xl jost_regular font-bold">
+            <h3 className=" text-4xl jost_regular font-bold max-md:text-3xl">
               Vision & Mission
             </h3>
           </div>
-          <div className="bottom-div-of-vision-and-mission w-full flex items-start justify-between">
-            <div className="left-div-of-bottom-div-of-vision-and-mission w-[48%] flex flex-col items-start justify-center gap-4">
-              <span className=" text-base underline jost_regular font-bold text-black">
+          <div className="bottom-div-of-vision-and-mission w-full flex items-start justify-between media-max-900:flex-col media-max-900:justify-center media-max-900:items-center media-max-900:gap-8">
+            <div className="left-div-of-bottom-div-of-vision-and-mission w-[48%] flex flex-col items-start justify-center gap-4 media-max-900:w-full media-max-900:gap-2">
+              <span className=" text-base underline jost_regular font-bold text-black max-md:text-sm">
                 Our Vision
               </span>
-              <p className="w-full  text-base text-gray-700 max-xlg2:w-10/12 max-xlg3:w-11/12 text-justify">
+              <p className="w-full  text-base text-gray-700 text-justify max-md:text-sm">
                 Located in New York, NY, Mindthera specializes in providing
                 top-notch psychotherapy services. Our team of experienced
                 professionals is dedicated to helping you achieve mental
@@ -28,8 +28,8 @@ function VisionAndMission() {
                 psychotherapy needs.
               </p>
             </div>
-            <div className="right-div-of-bottom-div-of-vision-and-mission w-[48%] flex flex-col items-start justify-center gap-4">
-              <span className=" text-base underline jost_regular font-bold text-black">
+            <div className="right-div-of-bottom-div-of-vision-and-mission w-[48%] flex flex-col items-start justify-center gap-4 media-max-900:w-full media-max-900:gap-2">
+              <span className=" text-base underline jost_regular font-bold text-black max-md:text-sm">
                 Our Mission
               </span>
               <ul className=" flex flex-col items-start justify-center gap-2 w-full">
@@ -37,15 +37,15 @@ function VisionAndMission() {
                   visionAndMissionContentData.map((d, i) => {
                     return (
                       <li className=" flex items-start justify-start gap-2 w-full relative">
-                        <span className=" absolute h-8 w-8 rounded-[50%] flex items-center justify-center bg-bg_color_secendory">
+                        <span className=" absolute h-8 w-8 rounded-[50%] flex items-center justify-center bg-bg_color_secendory max-md:text-sm">
                           {i + 1}
                         </span>
 
                         <p className=" ml-12 text-justify">
-                          <span className=" text-bg_color_primary text-base">
+                          <span className=" text-bg_color_primary text-base max-md:text-sm">
                             {d._name + " "}
                           </span>
-                          <span className=" text-base text-gray-700">
+                          <span className=" text-base text-gray-700 max-md:text-sm">
                             {" " + d._para}
                           </span>
                         </p>
--- app/components/who_we_are/index.tsx
+++ app/components/who_we_are/index.tsx
@@ -7,40 +7,40 @@ import { IoPlaySharp } from "react-icons/io5";
 
 function WhoWeAre() {
   return (
-    <main className=" w-full flex items-center justify-center py-16">
+    <main className=" w-full flex items-center justify-center py-16 mt-[72px]">
       <div className="who-we-are-container max-w-[1400px] w-full flex items-center justify-center">
-        <div className="who-we-are w-[82%] flex items-center justify-between">
-          <div className="left-div-of-who-we-are overflow-hidden rounded-3xl w-[48%] flex items-center justify-center">
-            <div className="img-div-of-left-div-of-who-we-are flex items-center bg-purple-100 justify-center w-full min-h-[350px] relative ">
+        <div className="who-we-are w-[84%] flex items-center justify-between media-max-900:flex-col-reverse media-max-900:justify-center media-max-900:gap-8 ">
+          <div className="left-div-of-who-we-are overflow-hidden rounded-3xl w-[48%] flex items-center justify-center media-max-900:w-full">
+            <div className="img-div-of-left-div-of-who-we-are flex items-center justify-center w-full min-h-[350px] relative media-max-500:min-h-64 media-max-362:min-h-44 ">
               <Image alt="img" className="h-full absolute" src={img1} />
 
               <button className="overly-border flex items-center justify-center absolute">
                 <Link
-                  className="popup-videos rounded-[50%] flex items-center justify-center relative text-center w-20 h-20 bg-bg_color_primary"
+                  className="popup-videos rounded-[50%] flex items-center justify-center relative text-center w-20 h-20 bg-bg_color_primary media-max-500:w-14 media-max-500:h-14 media-max-362:w-12 media-max-362:h-12"
                   href=""
                   target="_blank"
                 >
                   <span></span>
-                  <IoPlaySharp className=" text-5xl absolute text-gray-100 " />
+                  <IoPlaySharp className=" text-5xl absolute text-gray-100 media-max-500:text-4xl media-max-362:text-3xl " />
                 </Link>
               </button>
             </div>
           </div>
-          <div className="right-div-of-who-we-are min-h-[350px] w-[48%] flex flex-col items-start justify-between py-4">
-            <span className=" text-base underline jost_regular font-bold text-black">
+          <div className="right-div-of-who-we-are min-h-[350px] w-[48%] flex flex-col items-start justify-between py-4 media-max-900:w-full media-max-900:min-h-0 media-max-900:justify-center media-max-900:gap-4">
+            <span className=" text-base underline jost_regular font-bold text-black max-md:text-sm">
               Who We Are
             </span>
-            <h3 className="w-full text-4xl jost_regular font-bold">
+            <h3 className="w-full text-4xl jost_regular font-bold max-md:text-3xl">
               Introducing Our Expert Psychology Professionals
             </h3>
-            <p className="w-full  text-base text-gray-700 max-xlg2:w-10/12 max-xlg3:w-11/12 text-justify">
+            <p className=" w-full  text-base text-gray-700 text-justify max-md:text-sm">
               Located in New York, NY, Mindthera specializes in providing
               top-notch psychotherapy services. Our team of experienced
               professionals is dedicated to helping you achieve mental wellness
               and personal growth. Trust Mindthera for all your psychotherapy
               needs.
             </p>
-            <button className=" jost_regular text-white border-none rounded-3xl text-center bg-bg_color_primary px-6 py-2 text-sm font-bold cursor-pointer select-none hover:underline">
+            <button className=" jost_regular text-white border-none rounded-3xl text-center bg-bg_color_primary px-6 py-2 text-sm font-bold cursor-pointer select-none hover:underline max-md:text-xs">
               Get In Touch
             </button>
           </div>
